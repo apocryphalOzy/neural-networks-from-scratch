f02f06669a8e73fad0e82b5b8d9cf4bd5aa5fe3f
l337hax0r <lonesealpro@gmail.com>
2021-09-07 17:40:30 -0700

    Obtaining a product with vectors

--- chpt-1/layerOfNeurons.py
+++ chpt-1/layerOfNeurons.py
@@ -10,16 +10,33 @@ biases = [2, 3, .5]
 layer_outputs = []
 # For each neuron
 for neuron_weights, neuron_bias in zip(weights, biases): #destructuring
+
+    print('weights: ' )
+    print( neuron_weights)
+    print('biases: ' )
+    print( neuron_bias)
     #zeroed output of given neuron
     neuron_output = 0
+    print('outer loop' )
+    print(neuron_output)
     #For each input and weight to the neuron`
     for n_input, weight in zip(inputs, neuron_weights):
+
+        print('n_input ')
+        print(n_input)
+        print('weight ')
+        print(weight)
         # Multiply this input by associated weight
         # and add to the neuron's output variable
         neuron_output += n_input * weight
+
+        print('inner loop neuron output ')
+        print( neuron_output)
     
     #add bias to neuron output
     neuron_output += neuron_bias
+    print('neuron_output')
+    print(neuron_output)
     #put neuron's result to the layer's output list
     layer_outputs.append(neuron_output)
 
