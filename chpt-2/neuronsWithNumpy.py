5997994eed0bd48f3376c6e6562e431fee4d80e0
l337hax0r <lonesealpro@gmail.com>
2021-09-08 08:02:00 -0700

    coding a layer of neurons and getting output using NumPy dot product

--- chpt-2/neuronsWithNumpy.py
+++ chpt-2/neuronsWithNumpy.py
@@ -2,11 +2,23 @@
 
 import numpy as np
 
-inputs = [1,2,3,2.5]
-weights = [.2,.8,-.5,1]
-bias = 2
+# inputs = [1,2,3,2.5]
+# weights = [.2,.8,-.5,1]
+# bias = 2
 
-outputs = np.dot(weights, inputs) + bias
+# outputs = np.dot(weights, inputs) + bias
 
-print(outputs)
+# print(outputs)
 
+#=====================================================
+
+#layer of neurons using NumPy
+inputs = [1, 2, 3, 2.5]
+weights = [[.2, .8, -.5, 1],
+            [.5, -.91, .26, -.5],
+            [-.26, -.27, .17, .87]]
+biases = [2, 3, .5]
+
+layer_outputs = np.dot(weights, inputs) + biases
+
+print(layer_outputs)
